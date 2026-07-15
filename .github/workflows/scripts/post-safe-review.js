cb2329be87f8042868e7bd3f5964cd1386d05dae
Lance Hartung <hartung@cs.wisc.edu>
2026-07-15 14:55:29 -0500

    Trigger per-commit review on vague PRs

--- .github/workflows/scripts/post-safe-review.js
+++ .github/workflows/scripts/post-safe-review.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 
-// Template - pull request review, flagged for vagueness
+// Template: pull request review, positive for vagueness
 const vagueReport = (response) => `# Safe Pull Request Review
 
 1. **Vagueness:** ${response.vagueness.concerning ? "concerning" : "OK"}
@@ -11,7 +11,7 @@ const vagueReport = (response) => `# Safe Pull Request Review
 Code consistency could not be evaluated because the commit message was too vague. The pull request should be reviewed extra carefully for this reason.`;
 
 
-// Template - pull request review, not flagged for vagueness
+// Template: pull request review, negative for vagueness
 const nonvagueReport = (response) => `# Safe Pull Request Review
 
 1. **Vagueness:** ${response.vagueness.concerning ? "concerning" : "OK"}
@@ -29,7 +29,7 @@ const nonvagueReport = (response) => `# Safe Pull Request Review
 ${response.contradicting.concerning || response.incomplete.concerning ? "The pull request should be reviewed carefully for the reasons identified above." : "The pull request passes all code consistency checks. The changes should still be reviewed for desirability."}`;
 
 
-// Template - pull request review, error parsing model output
+// Template: pull request review, error parsing model output
 const parseErrorReport = (response) => `# Safe Pull Request Review
 
 ${response}
@@ -37,20 +37,20 @@ ${response}
 Note: an error occurred while parsing the report. The formatting may be incorrect, but the contents may still be helpful.`;
 
 
-// Template - commit review, flagged for vagueness
+// Template: commit review, positive for vagueness
 const vagueCommitReport = (response, hash, url, message, index) => `
-## ${index}. [${message[0]}](${url})
-${message[1] === "" ? "*Commit message has no further details*\n\n" : "<blockquote>" + message[1] + "</blockquote>"}
+## ${index}. [${message.subject}](${url})
+${message.body === "" ? "*Commit message has no further details*\n\n" : "<blockquote>" + message.body + "</blockquote>"}
 
 1. **Vagueness:** ${response.vagueness.concerning ? "concerning" : "OK"}
 
    ${response.vagueness.reason}`;
 
 
-// Template - commit review, not flagged for vagueness
+// Template: commit review, negative for vagueness
 const nonvagueCommitReport = (response, hash, url, message, index) => `
-## ${index}. [${message[0]}](${url})
-${message[1] === "" ? "*Commit message has no further details*\n\n" : "<blockquote>" + message[1] + "</blockquote>"}
+## ${index}. [${message.subject}](${url})
+${message.body === "" ? "*Commit message has no further details*\n\n" : "<blockquote>" + message.body + "</blockquote>"}
 
 1. **Vagueness:** ${response.vagueness.concerning ? "concerning" : "OK"}
 
@@ -65,37 +65,35 @@ ${message[1] === "" ? "*Commit message has no further details*\n\n" : "<blockquo
    ${response.incomplete.reason}`;
 
 
-// Template - commit review, error parsing model output
+// Template: commit review, error parsing model output
 const parseErrorReportCommit = (response, hash, url, message, index) => `
-## ${index}. [${message[0]}](${url})
-${message[1] === "" ? "*Commit message has no further details*\n\n" : "<blockquote>" + message[1] + "</blockquote>"}
+## ${index}. [${message.subject}](${url})
+${message.body === "" ? "*Commit message has no further details*\n\n" : "<blockquote>" + message.body + "</blockquote>"}
 
 ${response}
 
 Note: an error occurred while parsing the report. The formatting may be incorrect, but the contents may still be helpful.`;
 
 
-function parseGeminiOutput(response) {
-  // Gemini is probably trained on a lot of markdown, so it often includes code block signals.
-  if (response.startsWith("```json"))
-    response = response.slice(7);
-
-  response = response.trimEnd();
-  if (response.endsWith("```"))
-    response = response.slice(0, -3);
-
+// Parse JSON output from model
+function parseModelOutput(response) {
+  // Strip extraneous text before and after the JSON output.
+  var start = response.indexOf("{");
+  var end = response.lastIndexOf("}");
+  response = response.slice(start, end+1);
   return JSON.parse(response);
 }
 
 
+// Split commit message into subject line and body
 function splitCommitMessage(message) {
   const index = message.indexOf("\n");
   if (index < 0) {
-    return [message, ""];
+    return { subject: message, body: "" };
   } else {
     const subject = message.substring(0, index).trim();
     const body = message.substring(index+1).trim();
-    return [subject, body];
+    return { subject, body };
   }
 }
 
@@ -113,55 +111,17 @@ export async function postPullRequestReview(github, context, core) {
 
   var comment;
   try {
-    const response = parseGeminiOutput(geminiOutput);
+    const response = parseModelOutput(geminiOutput);
     if (response.vagueness.concerning) {
       comment = vagueReport(response);
     } else {
       comment = nonvagueReport(response);
     }
+    core.setOutput("should-review-commits", response.vagueness.concerning);
   } catch (error) {
     console.warn(error);
     comment = parseErrorReport(geminiOutput);
-  }
-
-  await github.rest.issues.createComment({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    issue_number: issueNumber,
-    body: comment
-  });
-}
-
-export async function postCommitReview(github, context, core) {
-  const commitHash = process.env.COMMIT_HASH;
-  const geminiOutput = process.env.GEMINI_OUTPUT;
-  const issueNumber = context.payload.pull_request
-    ? context.payload.pull_request.number
-    : context.payload.issue.number;
-
-  if (!issueNumber) {
-    core.setFailed("Could not determine the Issue or PR number.");
-    return;
-  }
-
-  const commits = JSON.parse(process.env.COMMITS);
-  const commitInfo = commits[commitHash].commit;
-
-  // I had some trouble with the html_url field in the commit info,
-  // so it may be more reliable to construct it from known information.
-  const url = `https://github.com/${context.repo.owner}/${context.repo.repo}/pull/${issueNumber}/changes/${commitHash}`
-
-  var comment;
-  try {
-    const response = parseGeminiOutput(geminiOutput);
-    if (response.vagueness.concerning) {
-      comment = vagueCommitReport(response, commitHash, url, commitInfo.message);
-    } else {
-      comment = nonvagueCommitReport(response, commitHash, url, commitInfo.message);
-    }
-  } catch (error) {
-    console.warn(error);
-    comment = parseErrorReportCommit(geminiOutput, commitHash, url, commitInfo.message);
+    core.setOutput("should-review-commits", true);
   }
 
   await github.rest.issues.createComment({
@@ -202,7 +162,7 @@ export async function postAggregateCommitReview(github, context, core) {
     const message = splitCommitMessage(commit.commit.message);
 
     try {
-      const response = parseGeminiOutput(output);
+      const response = parseModelOutput(output);
       vagueness = response.vagueness.concerning ? "concerning" : "OK";
       if (response.vagueness.concerning) {
         vagueness = "concerning";
@@ -221,7 +181,7 @@ export async function postAggregateCommitReview(github, context, core) {
       details += parseErrorReportCommit(response, hash, url, message, index);
     }
 
-    table += `| ${index} | ${message[0]} [${hash}](${url}) | ${vagueness} | ${contradicting} | ${incomplete} |\n`;
+    table += `| ${index} | ${message.subject} [${hash}](${url}) | ${vagueness} | ${contradicting} | ${incomplete} |\n`;
     index++;
   }
 
